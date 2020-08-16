f5bfcc17e36b03fa22c8c2ece97eac6804129d8e
radcleong94 <Radcleong@gmail.com>
2020-08-16 17:37:00 +0800

    Updates

---
+++ precache-manifest.8831eab4cf4651eda9eeb9c8b6e2389b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd95d68b5677ece7b9bb6dcf66170235",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "30d11c8ad65c1a557830",
+    "url": "/React-Calculator/static/css/main.46809b4d.chunk.css"
+  },
+  {
+    "revision": "48926d9c80cce50b0776",
+    "url": "/React-Calculator/static/js/2.2135fec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.2135fec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30d11c8ad65c1a557830",
+    "url": "/React-Calculator/static/js/main.dd173299.chunk.js"
+  },
+  {
+    "revision": "31f1b382c927167c40c0",
+    "url": "/React-Calculator/static/js/runtime-main.b6e8a5a6.js"
+  }
+]);
